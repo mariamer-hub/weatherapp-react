ac6a4c5b32cf668b0f5d447fefcf7304dc973575
Maria Mer <maria.mer@outlook.dk>
2023-05-15 23:56:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <Search defaultCity="Athens" />
+      <Search defaultCity="Aarhus" />
     </div>
   );
 }
